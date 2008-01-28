552f8dc3f16da3c2c609a38a4633c8316b4c38fd
Erick Tryzelaar <idadesub@users.sourceforge.net>
2008-01-27 18:21:35 -0800

    Add snprintf and vsnprintf tests.

--- lib/fbuild/builders/c/c99.py
+++ lib/fbuild/builders/c/c99.py
@@ -25,6 +25,55 @@ default_types_stdint_h = tuple('%sint%s%s_t' % (sign, attr, size)
 
 # -----------------------------------------------------------------------------
 
+def detect_snprintf(builder):
+    code = '''
+        #include <stdio.h>
+
+        int main(int argc,char** argv) {
+            char s[50];
+            int n = snprintf(s, 50, "%d %s\\n", 12345, "hello!");
+            return n!=13;
+        }
+    '''
+
+    builder.check('determing if snprintf is in stdio.h')
+    if builder.try_run(code):
+        builder.log('ok', color='green')
+        return True
+    else:
+        builder.log('failed', color='yellow')
+        return False
+
+
+def detect_vsnprintf(builder):
+    code = '''
+        #include <stdio.h>
+        #include <stdarg.h>
+
+        int check(char const*fmt,...)
+        {
+            va_list ap;
+            va_start(ap,fmt);
+            int n = vsnprintf(NULL,0,fmt,ap);
+            va_end(ap);
+            return n!=3;
+        }
+
+        int main(int argc,char** argv) {
+            return check("%s","XXX"); // 0 means pass
+        }
+    '''
+
+    builder.check('determing if vsnprintf is in stdio.h')
+    if builder.try_run(code):
+        builder.log('ok', color='green')
+        return True
+    else:
+        builder.log('failed', color='yellow')
+        return False
+
+# -----------------------------------------------------------------------------
+
 def config_types(conf, builder):
     conf.configure('c99.types',
         std.get_types_data, builder, default_types)
@@ -53,7 +102,12 @@ def config_stdint_h(conf, builder):
         std.get_types_data, builder, default_types_stdint_h,
         headers=['stdint.h'], int_type=True)
 
+def config_stdio_h(conf, builder):
+    conf.configure('c99.stdio_h.snprintf', detect_snprintf, builder)
+    conf.configure('c99.stdio_h.vsnprintf', detect_vsnprintf, builder)
+
 def config(conf, builder):
+    config_stdio_h(conf, builder)
     config_types(conf, builder)
     config_complex_h(conf, builder)
     config_stdbool_h(conf, builder)
