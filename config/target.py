4f9cf1fda312de10cac3ddca2543dd5655c7a6fb
Erick Tryzelaar <idadesub@users.sourceforge.net>
2008-01-28 20:30:43 -0800

    We aren't using these.

--- config/target.py
+++
@@ -1,7 +0,0 @@
-from fbuild.system import configure
-
-c_static_builder = configure('fbuild.builders.c.config_static', name='target')
-c_shared_builder = configure('fbuild.builders.c.config_shared', name='target')
-
-cxx_static_builder = configure('fbuild.builders.cxx.config_static', name='target')
-cxx_shared_builder = configure('fbuild.builders.cxx.config_shared', name='target')
