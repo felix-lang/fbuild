5c1a15a3d5dd5b0b36004f73a6a4609c70b48a3c
Erick Tryzelaar <idadesub@users.sourceforge.net>
2008-08-15 14:17:33 -0700

    Add a record class that allows dicts to be used like an object.
    This is to simplify the interface for dealing with nested dictionaries.
    Normally, you need to specify d['foo']['bar']['baz'], but it can be a lot
    easier to read as just d.foo.bar.baz, which this lightweight class provides.

--- lib/fbuild/__init__.py
+++ lib/fbuild/__init__.py
@@ -92,3 +92,19 @@ def execute(cmd,
         raise ExecutionError(cmd, stdout, stderr, returncode)
 
     return stdout, stderr
+
+# -----------------------------------------------------------------------------
+
+class Record(dict):
+    def __setattr__(self, key, value):
+        'Allow setting items as if they were attributes.'
+
+        self[key] = value
+
+    def __getattr__(self, key):
+        'Allow items to be exposed as attributes.'
+
+        try:
+            return self[key]
+        except KeyError as e:
+            raise AttributeError(e) from e
