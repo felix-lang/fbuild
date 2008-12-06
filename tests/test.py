ced643a6541a5952f7ec8715d1ad8b3de0e452ba
Erick Tryzelaar <idadesub@users.sourceforge.net>
2008-12-05 23:30:50 -0800

    Add doctests to the test suite.

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.0
 
+import os
 import sys
 import doctest
 import unittest
@@ -13,6 +14,28 @@ import test_scheduler
 
 def main():
     suite = unittest.TestSuite()
+
+    # Load the doctests
+    prefix = os.path.join('..', 'lib')
+    for root, dirs, files in os.walk(prefix):
+        root = root[len(prefix + os.sep):].replace(os.sep, '.')
+
+        for file in files:
+            if file == '__init__.py':
+                module = root
+            elif file.endswith('.py'):
+                module = root + '.' + file[:-len('.py')]
+            else:
+                continue
+
+            try:
+                test = doctest.DocTestSuite(__import__(module, {}, {}, ['']))
+            except ValueError as e:
+                # no doc test exists
+                pass
+            else:
+                suite.addTest(test)
+
     suite.addTest(test_functools.suite())
     suite.addTest(test_scheduler.suite())
 
