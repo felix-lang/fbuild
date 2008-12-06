b910cb729326cc3259ab7ee4801dd027736ec404
Erick Tryzelaar <idadesub@users.sourceforge.net>
2008-12-05 23:32:14 -0800

    Fix test_functools to use bind_args instead of _bind_args.

--- tests/test_functools.py
+++ tests/test_functools.py
@@ -1,6 +1,6 @@
 import unittest
 
-from fbuild.env import _bind_args
+from fbuild.functools import *
 
 # -----------------------------------------------------------------------------
 
@@ -9,153 +9,153 @@ class TestFunctionBind(unittest.TestCase):
         def f():
             pass
 
-        self.assertEquals(_bind_args(f, (), {}), {})
+        self.assertEquals(bind_args(f, (), {}), {})
 
-        self.assertRaises(TypeError, _bind_args, f, (1,), {})
-        self.assertRaises(TypeError, _bind_args, f, (),   {'x':1})
-        self.assertRaises(TypeError, _bind_args, f, (1,), {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,), {})
+        self.assertRaises(TypeError, bind_args, f, (),   {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,), {'x':1})
 
     def testSingle(self):
         def f(a):
             pass
 
-        self.assertEquals(_bind_args(f, (1,), {}),     {'a': 1})
-        self.assertEquals(_bind_args(f, (2,), {}),     {'a': 2})
-        self.assertEquals(_bind_args(f, (), {'a': 3}), {'a': 3})
+        self.assertEquals(bind_args(f, (1,), {}),     {'a': 1})
+        self.assertEquals(bind_args(f, (2,), {}),     {'a': 2})
+        self.assertEquals(bind_args(f, (), {'a': 3}), {'a': 3})
 
-        self.assertRaises(TypeError, _bind_args, f, (),     {})
-        self.assertRaises(TypeError, _bind_args, f, (1, 2), {})
-        self.assertRaises(TypeError, _bind_args, f, (),     {'x':1})
-        self.assertRaises(TypeError, _bind_args, f, (1,),   {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (),     {})
+        self.assertRaises(TypeError, bind_args, f, (1, 2), {})
+        self.assertRaises(TypeError, bind_args, f, (),     {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,),   {'x':1})
 
 
     def testMultiple(self):
         def f(a, b, c):
             pass
 
-        self.assertEquals(_bind_args(f, (1, 2, 3), {}), dict(a=1, b=2, c=3))
-        self.assertEquals(_bind_args(f, (2, 3, 4), {}), dict(a=2, b=3, c=4))
+        self.assertEquals(bind_args(f, (1, 2, 3), {}), dict(a=1, b=2, c=3))
+        self.assertEquals(bind_args(f, (2, 3, 4), {}), dict(a=2, b=3, c=4))
 
         self.assertEquals(
-            _bind_args(f, (), {'a': 1, 'b': 2, 'c': 3}),
+            bind_args(f, (), {'a': 1, 'b': 2, 'c': 3}),
             dict(a=1, b=2, c=3))
 
         self.assertEquals(
-            _bind_args(f, (1,), {'b': 2, 'c': 3}),
+            bind_args(f, (1,), {'b': 2, 'c': 3}),
             dict(a=1, b=2, c=3))
 
         self.assertEquals(
-            _bind_args(f, (1, 2), {'c': 3}),
+            bind_args(f, (1, 2), {'c': 3}),
             dict(a=1, b=2, c=3))
 
-        self.assertRaises(TypeError, _bind_args, f, (),           {})
-        self.assertRaises(TypeError, _bind_args, f, (1, 2),       {})
-        self.assertRaises(TypeError, _bind_args, f, (1, 2, 3, 4), {})
-        self.assertRaises(TypeError, _bind_args, f, (),           {'x':1})
-        self.assertRaises(TypeError, _bind_args, f, (1,),         {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (),           {})
+        self.assertRaises(TypeError, bind_args, f, (1, 2),       {})
+        self.assertRaises(TypeError, bind_args, f, (1, 2, 3, 4), {})
+        self.assertRaises(TypeError, bind_args, f, (),           {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,),         {'x':1})
 
     def testDefault(self):
         def f(a, b, c=8, d=9):
             pass
 
         self.assertEquals(
-            _bind_args(f, (1, 2), {}),
+            bind_args(f, (1, 2), {}),
             dict(a=1, b=2, c=8, d=9))
 
         self.assertEquals(
-            _bind_args(f, (1, 2, 4), {}),
+            bind_args(f, (1, 2, 4), {}),
             dict(a=1, b=2, c=4, d=9))
 
         self.assertEquals(
-            _bind_args(f, (1, 2, 3, 4), {}),
+            bind_args(f, (1, 2, 3, 4), {}),
             dict(a=1, b=2, c=3, d=4))
 
         self.assertEquals(
-            _bind_args(f, (), {'a': 1, 'b': 2, 'c': 3, 'd': 4}),
+            bind_args(f, (), {'a': 1, 'b': 2, 'c': 3, 'd': 4}),
             dict(a=1, b=2, c=3, d=4))
 
         self.assertEquals(
-            _bind_args(f, (1,), {'b': 2, 'c': 3, 'd': 4}),
+            bind_args(f, (1,), {'b': 2, 'c': 3, 'd': 4}),
             dict(a=1, b=2, c=3, d=4))
 
         self.assertEquals(
-            _bind_args(f, (1, 2), {'c': 3, 'd': 4}),
+            bind_args(f, (1, 2), {'c': 3, 'd': 4}),
             dict(a=1, b=2, c=3, d=4))
 
         self.assertEquals(
-            _bind_args(f, (1, 2, 3), {'d': 4}),
+            bind_args(f, (1, 2, 3), {'d': 4}),
             dict(a=1, b=2, c=3, d=4))
 
-        self.assertRaises(TypeError, _bind_args, f, (),              {})
-        self.assertRaises(TypeError, _bind_args, f, (1,),            {})
-        self.assertRaises(TypeError, _bind_args, f, (1, 2, 3, 4, 5), {})
-        self.assertRaises(TypeError, _bind_args, f, (),              {'x':1})
-        self.assertRaises(TypeError, _bind_args, f, (1,),            {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (),              {})
+        self.assertRaises(TypeError, bind_args, f, (1,),            {})
+        self.assertRaises(TypeError, bind_args, f, (1, 2, 3, 4, 5), {})
+        self.assertRaises(TypeError, bind_args, f, (),              {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,),            {'x':1})
 
     def testVarArgs0(self):
         def f(*args):
             pass
 
-        self.assertEquals(_bind_args(f, (), {}),        {})
-        self.assertEquals(_bind_args(f, (1, 2, 4), {}), {'args': (1, 2, 4)})
+        self.assertEquals(bind_args(f, (), {}),        {})
+        self.assertEquals(bind_args(f, (1, 2, 4), {}), {'args': (1, 2, 4)})
 
-        self.assertRaises(TypeError, _bind_args, f, (),   {'x':1})
-        self.assertRaises(TypeError, _bind_args, f, (1,), {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (),   {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,), {'x':1})
 
     def testVarArgs1(self):
         def f(a, *args):
             pass
 
-        self.assertEquals(_bind_args(f, (1,), {}),      dict(a=1))
-        self.assertEquals(_bind_args(f, (), {'a': 1}),  dict(a=1))
-        self.assertEquals(_bind_args(f, (1, 2, 4), {}), dict(a=1, args=(2, 4)))
+        self.assertEquals(bind_args(f, (1,), {}),      dict(a=1))
+        self.assertEquals(bind_args(f, (), {'a': 1}),  dict(a=1))
+        self.assertEquals(bind_args(f, (1, 2, 4), {}), dict(a=1, args=(2, 4)))
 
-        self.assertRaises(TypeError, _bind_args, f, (),   {})
-        self.assertRaises(TypeError, _bind_args, f, (2, 4), {'a': 1})
-        self.assertRaises(TypeError, _bind_args, f, (),   {'x':1})
-        self.assertRaises(TypeError, _bind_args, f, (1,), {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (),   {})
+        self.assertRaises(TypeError, bind_args, f, (2, 4), {'a': 1})
+        self.assertRaises(TypeError, bind_args, f, (),   {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,), {'x':1})
 
     def testVarArgs2(self):
         def f(a, b, c=8, d=9, *args):
             pass
 
         self.assertEquals(
-            _bind_args(f, (1, 2), {}),
+            bind_args(f, (1, 2), {}),
             dict(a=1, b=2, c=8, d=9))
 
         self.assertEquals(
-            _bind_args(f, (1, 2, 4), {}),
+            bind_args(f, (1, 2, 4), {}),
             dict(a=1, b=2, c=4, d=9))
 
         self.assertEquals(
-            _bind_args(f, (1, 2, 3, 4), {}),
+            bind_args(f, (1, 2, 3, 4), {}),
             dict(a=1, b=2, c=3, d=4))
 
         self.assertEquals(
-            _bind_args(f, (1, 2, 3, 4, 5), {}),
+            bind_args(f, (1, 2, 3, 4, 5), {}),
             dict(a=1, b=2, c=3, d=4, args=(5,)))
 
         self.assertEquals(
-            _bind_args(f, (), {'a': 1, 'b': 2, 'c': 3, 'd': 4}),
+            bind_args(f, (), {'a': 1, 'b': 2, 'c': 3, 'd': 4}),
             dict(a=1, b=2, c=3, d=4))
 
         self.assertEquals(
-            _bind_args(f, (1,), {'b': 2, 'c': 3, 'd': 4}),
+            bind_args(f, (1,), {'b': 2, 'c': 3, 'd': 4}),
             dict(a=1, b=2, c=3, d=4))
 
         self.assertEquals(
-            _bind_args(f, (1, 2), {'c': 3, 'd': 4}),
+            bind_args(f, (1, 2), {'c': 3, 'd': 4}),
             dict(a=1, b=2, c=3, d=4))
 
         self.assertEquals(
-            _bind_args(f, (1, 2, 3), {'d': 4}),
+            bind_args(f, (1, 2, 3), {'d': 4}),
             dict(a=1, b=2, c=3, d=4))
 
-        self.assertRaises(TypeError, _bind_args, f, (),   {})
-        self.assertRaises(TypeError, _bind_args, f, (1,), {})
-        self.assertRaises(TypeError, _bind_args, f, (),   {'x':1})
-        self.assertRaises(TypeError, _bind_args, f, (1,), {'x':1})
-        self.assertRaises(TypeError, _bind_args, f,
+        self.assertRaises(TypeError, bind_args, f, (),   {})
+        self.assertRaises(TypeError, bind_args, f, (1,), {})
+        self.assertRaises(TypeError, bind_args, f, (),   {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,), {'x':1})
+        self.assertRaises(TypeError, bind_args, f,
             (1, 2, 3, 4, 5), {'a': 1, 'b': 2, 'c': 3, 'd': 4})
 
     def testKeywordOnly(self):
@@ -163,58 +163,58 @@ class TestFunctionBind(unittest.TestCase):
             pass
 
         self.assertEquals(
-            _bind_args(f, (), {'a': 1, 'b': 2}),
+            bind_args(f, (), {'a': 1, 'b': 2}),
             dict(a=1, b=2))
 
-        self.assertRaises(TypeError, _bind_args, f, (),   {})
-        self.assertRaises(TypeError, _bind_args, f, (1,), {})
-        self.assertRaises(TypeError, _bind_args, f, (),   {'x':1})
-        self.assertRaises(TypeError, _bind_args, f, (1,), {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (),   {})
+        self.assertRaises(TypeError, bind_args, f, (1,), {})
+        self.assertRaises(TypeError, bind_args, f, (),   {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,), {'x':1})
 
     def testKeywordDefaults(self):
         def f(*, a, b=8, c=9):
             pass
 
         self.assertEquals(
-            _bind_args(f, (), {'a': 1}),
+            bind_args(f, (), {'a': 1}),
             dict(a=1, b=8, c=9))
 
         self.assertEquals(
-            _bind_args(f, (), {'a': 1, 'b': 2, 'c': 3}),
+            bind_args(f, (), {'a': 1, 'b': 2, 'c': 3}),
             dict(a=1, b=2, c=3))
 
         self.assertEquals(
-            _bind_args(f, (), {'a': 1, 'b': 2}),
+            bind_args(f, (), {'a': 1, 'b': 2}),
             dict(a=1, b=2, c=9))
 
-        self.assertRaises(TypeError, _bind_args, f, (1,), {})
-        self.assertRaises(TypeError, _bind_args, f, (),   {'x':1})
-        self.assertRaises(TypeError, _bind_args, f, (1,), {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,), {})
+        self.assertRaises(TypeError, bind_args, f, (),   {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,), {'x':1})
 
     def testVarKeyword0(self):
         def f(**kwargs):
             pass
 
-        self.assertEquals(_bind_args(f, (), {}),       {})
-        self.assertEquals(_bind_args(f, (), {'a': 1}), {'a':1})
+        self.assertEquals(bind_args(f, (), {}),       {})
+        self.assertEquals(bind_args(f, (), {'a': 1}), {'a':1})
 
         self.assertEquals(
-            _bind_args(f, (), {'a': 1, 'b': 2, 'c': 3}),
+            bind_args(f, (), {'a': 1, 'b': 2, 'c': 3}),
             dict(a=1, b=2, c=3))
 
-        self.assertRaises(TypeError, _bind_args, f, (1,), {})
-        self.assertRaises(TypeError, _bind_args, f, (1,), {'x':1})
+        self.assertRaises(TypeError, bind_args, f, (1,), {})
+        self.assertRaises(TypeError, bind_args, f, (1,), {'x':1})
 
     def testAll(self):
         def f(a, b, c=7, d=8, *args, e, f, g=9, h=0, **kwargs):
             pass
 
         self.assertEquals(
-            _bind_args(f, (1, 2), {'e': 3, 'f': 4}),
+            bind_args(f, (1, 2), {'e': 3, 'f': 4}),
             dict(a=1, b=2, c=7, d=8, e=3, f=4, g=9, h=0))
 
         self.assertEquals(
-            _bind_args(f, (1, 2, 3, 4, 5), dict(e=3, f=4, g=5, h=6, i=7)),
+            bind_args(f, (1, 2, 3, 4, 5), dict(e=3, f=4, g=5, h=6, i=7)),
             dict(a=1, b=2, c=3, d=4, e=3, f=4, g=5, h=6, i=7, args=(5,)))
 
 # -----------------------------------------------------------------------------
