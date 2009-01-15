e710d687c4478da420b6be8187e1b5e371853856
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-01-15 09:59:14 -0800

    Add support for the bsd sys/mman.h extensions.

--- lib/fbuild/config/c/bsd.py
+++ lib/fbuild/config/c/bsd.py
@@ -1,4 +1,5 @@
 import fbuild.config.c as c
+import fbuild.config.c.posix04 as posix04
 
 # ------------------------------------------------------------------------------
 
@@ -15,3 +16,26 @@ class sys_event_h(c.Header):
             return (-1 == kq) ? 1 : 0;
         }
         ''')
+
+class sys_mman_h(posix04.sys_mman_h):
+    MAP_ANON = c.macro_test()
+    MAP_COPY = c.macro_test()
+    MAP_HASSEMAPHORE = c.macro_test()
+    MAP_NOCACHE = c.macro_test()
+    MAP_NOEXTEND = c.macro_test()
+    MAP_NORESERVE = c.macro_test()
+    MAP_RENAME = c.macro_test()
+    MADV_NORMAL = c.macro_test()
+    MADV_RANDOM = c.macro_test()
+    MADV_SEQUENTIAL = c.macro_test()
+    MADV_WILLNEED = c.macro_test()
+    MADV_DONTNEED = c.macro_test()
+    MADV_FREE = c.macro_test()
+    MINCORE_INCORE = c.macro_test()
+    MINCORE_REFERENCED = c.macro_test()
+    MINCORE_MODIFIED = c.macro_test()
+    MINCORE_REFERENCED_OTHER = c.macro_test()
+    MINCORE_MODIFIED_OTHER = c.macro_test()
+    madvise = c.function_test('void*', 'size_t', 'int')
+    mincore = c.function_test('int', 'const void*', 'size_t', 'char*')
+    minherit = c.function_test('void*', 'size_t', 'int')
