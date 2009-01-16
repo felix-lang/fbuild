30a1c82b937ca13ee61cdd54ecc0c828de6729a4
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-01-16 01:22:55 -0800

    Add configs for bsd execinfo.h, sys/ndir.h, stdlib.h, sys/dir.h, and unistd.h.

--- lib/fbuild/config/c/bsd.py
+++ lib/fbuild/config/c/bsd.py
@@ -3,6 +3,41 @@ import fbuild.config.c.posix04 as posix04
 
 # ------------------------------------------------------------------------------
 
+class execinfo_h(c.Header):
+    backtrace = c.function_test('int', 'void**', 'int', test='''
+        #include <execinfo.h>
+        int main() {
+            void* callstack[128];
+            int frames = backtrace(callstack, 128);
+            char** strs = backtrace_symbols(callstack, frames);
+            return 0;
+        }
+        ''')
+    backtrace_symbols = c.function_test('char**', 'void* const*', 'int',
+        test=backtrace.test)
+    backtrace_symbols_fd = c.function_test('void', 'void* const*', 'int', 'int')
+
+class stdlib_h(posix04.stdlib_h):
+    strtof = c.function_test('double', 'const char*', 'char**', test='''
+        #include <stdlib.h>
+        int main() {
+            char* s1 = "15";
+            char* s2 = "abc";
+            char* endp;
+            quad_t d = strtoq(s1, &endp);
+            if (s1 != endp && *endp == '\0' && d == 15.0) {
+                d = strtoq(s2, &endp);
+                return s1 == endp || *endp != '\0' ? 0 : 1;
+            }
+            return 1;
+        }
+        ''')
+
+class sys_dir_h(c.Header):
+    header = 'sys/dir.h'
+
+    DIR = c.type_test()
+
 class sys_event_h(c.Header):
     header = 'sys/event.h'
 
@@ -18,6 +53,8 @@ class sys_event_h(c.Header):
         ''')
 
 class sys_mman_h(posix04.sys_mman_h):
+    header = 'sys/mman.h'
+
     MAP_ANON = c.macro_test()
     MAP_COPY = c.macro_test()
     MAP_HASSEMAPHORE = c.macro_test()
@@ -39,3 +76,13 @@ class sys_mman_h(posix04.sys_mman_h):
     madvise = c.function_test('void*', 'size_t', 'int')
     mincore = c.function_test('int', 'const void*', 'size_t', 'char*')
     minherit = c.function_test('void*', 'size_t', 'int')
+
+class sys_ndir_h(c.Header):
+    header = 'sys/ndir.h'
+
+class sys_param_h(c.Header):
+    header = 'sys/param.h'
+
+class unistd_h(posix04.unistd_h):
+    brk = c.function_test('int', 'void*')
+    sbrk = c.function_test('void*', 'intptr_t')
