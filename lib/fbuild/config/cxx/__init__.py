6c4f9bcb60c46e38d327175cd6b1b91c5ea76b49
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-01-16 01:25:06 -0800

    Add config support for testing c++ templates.

--- lib/fbuild/config/cxx/__init__.py
+++ lib/fbuild/config/cxx/__init__.py
@@ -1,5 +1,10 @@
+import textwrap
+
+import fbuild
 import fbuild.config.c as c
 import fbuild.config.c.c90 as c90
+
+# ------------------------------------------------------------------------------
 
 function_test = c.function_test
 macro_test = c.macro_test
@@ -15,6 +20,50 @@ class struct_test(c.struct_test):
         cacheproperty(self).contribute_to_class(cls, key)
 
 variable_test = c.variable_test
+
+# ------------------------------------------------------------------------------
+
+class Template:
+    """L{Template} describes the features of a template.  It contains no
+    data."""
+
+    def __repr__(self):
+        return '%s()' % self.__class__.__name__
+
+    def __eq__(self, other):
+        return type(self) is type(other)
+
+    def __hash__(self):
+        return hash(self.__class__)
+
+class template_test(c.AbstractFieldDescriptor):
+    """template_test is a descriptor that tests for the template on the first
+    access. If it exists, an instance of L{Template} is memoized in the object
+    and returned. Otherwise, memoize and return None."""
+
+    def __init__(self, *, test_types=[], **kwargs):
+        super().__init__(**kwargs)
+        self.test_types = test_types
+
+    def format_test(self, header=None):
+        return textwrap.dedent('''
+            %s
+
+            int main() {
+                %s<%s> t;
+                return 0;
+            }
+        ''') % (
+            '' if header is None else '#include <%s>' % header,
+            self.name,
+            ', '.join(self.test_types))
+
+    def process_stdout(self, stdout):
+        if self.stdout is None or self.stdout == stdout:
+            fbuild.logger.passed()
+            return Template()
+
+        fbuild.logger.failed()
 
 # ------------------------------------------------------------------------------
 
