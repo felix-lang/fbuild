ad975dc5800fae7f79f8bfc7e30c3afc63d2f733
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-01-17 01:27:56 -0800

    Add a simple config for darwin headers.

---
+++ lib/fbuild/config/c/darwin.py
@@ -0,0 +1,9 @@
+import fbuild.config.c as c
+
+# ------------------------------------------------------------------------------
+
+class mach_mach_h(c.Header):
+    header = 'mach/mach.h'
+
+class mach_o_dyld_h(c.Header):
+    header = 'mach-o/dyld.h'
