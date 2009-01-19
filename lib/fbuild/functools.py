147de71349cf71bc35810c18dc89fbab1497e4b7
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-01-18 16:52:25 -0800

    Add a function that simplifies importing a module dynamically.

--- lib/fbuild/functools.py
+++ lib/fbuild/functools.py
@@ -5,12 +5,19 @@ import types
 
 # ------------------------------------------------------------------------------
 
+def import_module(module):
+    """L{import_module} is a shortcut that will import and return L{module}
+    if it is a I{str}. If not, then it is assumed that L{module} is an actual
+    module, and it is returned."""
+
+    if isinstance(module, str):
+        return __import__(module, {}, {}, [''])
+    return module
+
 def import_function(function):
-    '''
-    L{import_function} is a shortcut that will import and return L{function} if
-    it is a I{str}. If not, then it is assumed that L{function} is callable,
-    and is returned.
-    '''
+    """L{import_function} is a shortcut that will import and return L{function}
+    if it is a I{str}. If not, then it is assumed that L{function} is callable,
+    and is returned."""
 
     if isinstance(function, str):
         m, f = function.rsplit('.', 1)
@@ -18,11 +25,9 @@ def import_function(function):
     return function
 
 def call(function, *args, **kwargs):
-    '''
-    L{call} is a shortcut that will call L{function} with the specified
+    """L{call} is a shortcut that will call L{function} with the specified
     arguments. If L{function} is a I{str}, it is imported first. This
-    eliminates the need to import the module directly.
-    '''
+    eliminates the need to import the module directly."""
 
     return import_function(function)(*args, **kwargs)
 
