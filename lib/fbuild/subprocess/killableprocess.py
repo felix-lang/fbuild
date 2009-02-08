896b6e9ef8f803224b6fa5103a23d07105735a47
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-02-07 20:38:33 -0800

    Sync killableprocess with python 3.0's subprocess module.

--- lib/fbuild/subprocess/killableprocess.py
+++ lib/fbuild/subprocess/killableprocess.py
@@ -133,6 +133,21 @@ class Popen(subprocess.Popen):
                 startupinfo.wShowWindow = winprocess.SW_HIDE
                 comspec = os.environ.get("COMSPEC", "cmd.exe")
                 args = comspec + " /c " + args
+                if (GetVersion() >= 0x80000000 or
+                        os.path.basename(comspec).lower() == "command.com"):
+                    # Win9x, or using command.com on NT. We need to
+                    # use the w9xpopen intermediate program. For more
+                    # information, see KB Q150956
+                    # (http://web.archive.org/web/20011105084002/http://support.microsoft.com/support/kb/articles/Q150/9/56.asp)
+                    w9xpopen = self._find_w9xpopen()
+                    args = '"%s" %s' % (w9xpopen, args)
+                    # Not passing CREATE_NEW_CONSOLE has been known to
+                    # cause random failures on win9x.  Specifically a
+                    # dialog: "Your program accessed mem currently in
+                    # use at xxx" and a hopeful warning about the
+                    # stability of your system.  Cost is Ctrl+C wont
+                    # kill children.
+                    creationflags |= CREATE_NEW_CONSOLE
 
             # We create a new job for this process, so that we can kill
             # the process and any sub-processes
@@ -141,13 +156,14 @@ class Popen(subprocess.Popen):
             creationflags |= winprocess.CREATE_SUSPENDED
             creationflags |= winprocess.CREATE_UNICODE_ENVIRONMENT
 
-            hp, ht, pid, tid = winprocess.CreateProcess(
-                executable, args,
-                None, None, # No special security
-                1, # Must inherit handles!
+            hp, ht, pid, tid = winprocess.CreateProcess(executable, args,
+                # no special security
+                None, None,
+                int(not close_fds),
                 creationflags,
                 winprocess.EnvironmentBlock(env),
-                cwd, startupinfo)
+                cwd,
+                startupinfo)
 
             self._child_created = True
             self._handle = hp
