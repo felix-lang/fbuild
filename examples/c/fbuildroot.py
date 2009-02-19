501cc35acd564513ae9f9d7b2892d69bc9305062
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-02-19 15:17:40 -0800

    Run the c example automatically.

--- examples/c/fbuildroot.py
+++ examples/c/fbuildroot.py
@@ -1,9 +1,8 @@
+import fbuild
 import fbuild.builders.c
 
 def build():
     static = fbuild.builders.c.guess_static()
-    shared = fbuild.builders.c.guess_shared()
-
     lib1 = static.build_lib('static1', ['lib1/lib1.c'])
     lib2 = static.build_lib('static2', ['lib2/lib2.c'],
         includes=['lib1'], libs=[lib1])
@@ -13,8 +12,15 @@ def build():
     exe = static.build_exe('static', ['exe.c'],
         includes=['lib1', 'lib2'], libs=[lib2])
 
+    fbuild.logger.log(' * running %s:' % exe)
+    fbuild.execute([exe])
+
+    shared = fbuild.builders.c.guess_shared()
     lib1 = shared.build_lib('shared1', ['lib1/lib1.c'])
     lib2 = shared.build_lib('shared2', ['lib2/lib2.c'],
         includes=['lib1'], libs=[lib1])
     exe = shared.build_exe('shared', ['exe.c'],
         includes=['lib1', 'lib2'], libs=[lib2])
+
+    fbuild.logger.log(' * running %s:' % exe)
+    fbuild.execute([exe])
