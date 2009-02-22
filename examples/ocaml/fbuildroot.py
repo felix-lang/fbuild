05b14e55cf67df3d69fcba75d2d5fd60a65c750f
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-02-21 17:30:09 -0800

    Execute the ocaml exes.

--- examples/ocaml/fbuildroot.py
+++ examples/ocaml/fbuildroot.py
@@ -8,10 +8,17 @@ def build():
     liba = ocaml.ocamlc.build_lib('liba', Path.glob('a*.ml{,i}'), libs=[libb])
     exe  = ocaml.ocamlc.build_exe('exe.byte', ['exe.ml'], libs=[libb, liba])
 
+    fbuild.logger.log(' * running %s:' % exe)
+    fbuild.execute([exe])
+
     libb = ocaml.ocamlopt.build_lib('libb', Path.glob('b*.ml{,i}'))
     liba = ocaml.ocamlopt.build_lib('liba', Path.glob('a*.ml{,i}'), libs=[libb])
     exe  = ocaml.ocamlopt.build_exe('exe.native', ['exe.ml'], libs=[libb, liba])
 
+    fbuild.logger.log(' * running %s:' % exe)
+    fbuild.execute([exe])
+
+    # We can also build bytecode and native libraries at the same time.
     libb = ocaml.build_lib('libb', Path.glob('b*.ml{,i}'))
     liba = ocaml.build_lib('liba', Path.glob('a*.ml{,i}'), libs=[libb])
     exe  = ocaml.build_exe('exe', ['exe.ml'], libs=[libb, liba]).bytecode
