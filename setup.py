57c202125981a1d6f933655577548a8a4fca8035
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-06-20 09:40:24 -0700

    Add a distutils setup.py to prep for release.
    Eventually it'd be nice to do the install from inside fbuild,
    but until that's ready we'll use distutils for installing
    fbuild.

---
+++ setup.py
@@ -0,0 +1,29 @@
+import sys
+from distutils.core import setup
+
+sys.path.append('lib')
+import fbuild
+
+setup(
+    name='fbuild',
+    version=fbuild.__version__,
+    description='fbuild build system',
+    author='Erick Tryzelaar',
+    author_email='erickt@felix-lang.org',
+    url='http://felix-lang.org',
+    license='BSD',
+    packages=[
+        'fbuild',
+        'fbuild.builders',
+        'fbuild.builders.c',
+        'fbuild.builders.c.gcc',
+        'fbuild.builders.cxx',
+        'fbuild.builders.cxx.gxx',
+        'fbuild.config',
+        'fbuild.config.c',
+        'fbuild.config.cxx',
+        'fbuild.subprocess',
+    ],
+    scripts=['bin/fbuild'],
+    package_dir={'': 'lib'},
+)
