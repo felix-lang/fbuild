a4bc852d0cdbc446fc2e2d1375cd264458201fce
Austin Seipp <aseipp@existential.local>
2009-11-05 02:05:52 -0600

    add openssl support to fbuild.config.c

---
+++ lib/fbuild/config/c/openssl.py
@@ -0,0 +1,87 @@
+import fbuild.config.c as c
+
+# ------------------------------------------------------------------------------
+
+class ssl_h(c.Header):
+    header = 'openssl/ssl.h'
+
+class crypto_h(c.Header):
+    header = 'openssl/crypto.h'
+
+class blowfish_h(c.Header):
+    header = 'openssl/blowfish.h'
+
+class des_h(c.Header):
+    header = 'openssl/des.h'
+
+class rc4_h(c.Header):
+    header = 'openssl/rc4.h'
+
+class dsa_h(c.Header):
+    header = 'openssl/dsa.h'
+
+class dh_h(c.Header):
+    header = 'openssl/dh.h'
+
+class engine_h(c.Header):
+    header = 'openssl/engine.h'
+
+class rsa_h(c.Header):
+    header = 'openssl/rsa.h'
+
+class x509_h(c.Header):
+    header = 'openssl/x509.h'
+
+class hmac_h(c.Header):
+    header = 'openssl/hmac.h'
+
+class md2_h(c.Header):
+    header = 'openssl/md2.h'
+
+class md3_h(c.Header):
+    header = 'openssl/md3.h'
+
+class md4_h(c.Header):
+    header = 'openssl/md4.h'
+
+class md5_h(c.Header):
+    header = 'openssl/md5.h'
+
+class mdc2_h(c.Header):
+    header = 'openssl/mdc2.h'
+
+class ripemd_h(c.Header):
+    header = 'openssl/ripemd.h'
+
+class sha_h(c.Header):
+    header = 'openssl/sha.h'
+
+class err_h(c.Header):
+    header = 'openssl/err.h.h'
+
+class threads_h(c.Header):
+    header = 'openssl/threads.h'
+
+class rand_h(c.Header):
+    header = 'openssl/rand.h'
+
+class opensslv_h(c.Header):
+    header = 'openssl/opensslv.h'
+
+class bio_h(c.Header):
+    header = 'openssl/bio.h'
+
+class evp_h(c.Header):
+    header = 'openssl/evp.h'
+
+class pem_h(c.Header):
+    header = 'openssl/pem.h'
+
+class bn_h(c.Header):
+    header = 'openssl/bn.h'
+
+class buffer_h(c.Header):
+    header = 'openssl/buffer.h'
+
+class lhash_h(c.Header):
+    header = 'openssl/lhash.h'
