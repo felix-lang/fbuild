9de10bc6015fe34280a0c48d58fef6b95f265e31
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2011-03-31 21:12:45 -0700

    Fix the iphone g++ builder.

--- lib/fbuild/builders/cxx/gxx/iphone.py
+++ lib/fbuild/builders/cxx/gxx/iphone.py
@@ -3,28 +3,40 @@ import fbuild.builders.c.gcc.iphone as iphone
 
 # ------------------------------------------------------------------------------
 
-def static(exe=None, *args, **kwargs):
+def static(ctx, exe=None, *args, **kwargs):
     if exe is None:
         exe = iphone._iphone_devroot(False) / 'usr/bin/g++'
 
-    return iphone._builder(darwin.static, exe, *args, simulator=False, **kwargs)
+    print('g++.iphone.static.exe=', exe)
 
-def shared(exe=None, *args, **kwargs):
+    return iphone._builder(darwin.static, ctx, exe, *args,
+        simulator=False,
+        cross_compiler=True,
+        **kwargs)
+
+def shared(ctx, exe=None, *args, **kwargs):
     if exe is None:
         exe = iphone._iphone_devroot(False) / 'usr/bin/g++'
 
-    return iphone._builder(darwin.shared, exe, *args, simulator=False, **kwargs)
+    return iphone._builder(darwin.shared, ctx, exe, *args,
+        simulator=False,
+        cross_compiler=True,
+        **kwargs)
 
 # ------------------------------------------------------------------------------
 
-def static_simulator(exe=None, *args, **kwargs):
+def static_simulator(ctx, exe=None, *args, **kwargs):
     if exe is None:
         exe = iphone._iphone_devroot(True) / 'usr/bin/g++'
 
-    return iphone._builder(darwin.static, exe, *args, simulator=True, **kwargs)
+    return iphone._builder(darwin.static, ctx, exe, *args,
+        simulator=True,
+        **kwargs)
 
-def shared_simulator(exe=None, *args, **kwargs):
+def shared_simulator(ctx, exe=None, *args, **kwargs):
     if exe is None:
         exe = iphone._iphone_devroot(True) / 'usr/bin/g++'
 
-    return iphone._builder(darwin.shared, exe, *args, simulator=True, **kwargs)
+    return iphone._builder(darwin.shared, ctx, exe, *args,
+        simulator=True,
+        **kwargs)
