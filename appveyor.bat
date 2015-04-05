4cabb02d772b302845a350616b3c61547467c1fc
Ryan Gonzalez <rymg19@gmail.com>
2015-04-04 19:13:21 -0500

    Simplify AppVeyor tests for now (the OCaml installer doesn't work)

--- appveyor.bat
+++ appveyor.bat
@@ -2,5 +2,7 @@ set ERROR=0
 cd tests
 C:/Python34/python.exe run_tests.py
 cd ../examples
-for /D %d in (*) do C:/Python34/Scripts/fbuild.py || set ERROR=1
+:: for /D %d in (*) do C:/Python34/Scripts/fbuild.py || set ERROR=1
+cd c
+C:/Python34/python.exe ../../fbuild-light
 exit %ERROR%
